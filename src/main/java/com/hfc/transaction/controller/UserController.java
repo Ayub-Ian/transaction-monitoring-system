73406d17f1ae707ad34add5809a32533e14bde2e
Ayub Moronge <ayub.moronge@atlancis.com>
2025-06-03 14:42:34 +0300

    feat: update application yml db_url

--- src/main/java/com/hfc/transaction/controller/UserController.java
+++ src/main/java/com/hfc/transaction/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
+
     private UserRepository userRepository;
 
     @GetMapping
